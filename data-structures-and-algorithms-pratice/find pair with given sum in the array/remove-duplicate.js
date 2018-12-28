8c5bf117a2e0d94cc8336fe10ce3b3539557a514
Kevin Kim <kevinlabx@gmail.com>
2018-12-28 13:37:00 -0600

    Add remove duplicate to `find pair with given sum in the array`

---
+++ data-structures-and-algorithms-pratice/find pair with given sum in the array/remove-duplicate.js
@@ -0,0 +1,29 @@
+function findPair(arr, sum) {
+  var pairs = {};
+
+  if (arr.length > 0 && sum !== undefined) {
+    for (let i = 0; i < arr.length; i++) {
+      for (let j = 0; j < arr.length; j++) {
+
+        if (arr[i] + arr[j] === sum) {
+          if (pairs[arr[j]] === arr[i]) {
+            break;
+          } else {
+            pairs[arr[i]] = arr[j];
+          }
+          console.log(`Pair found at index ${i} and ${j} (${arr[i]}+${arr[j]})`);
+        }
+
+      }
+    }
+  }
+}
+
+var arr = [8, 7, 2, 5, 3, 1];
+var sum = 10;
+
+findPair(arr, sum);
+
+
+// This approach remove the duplicate pairs.
+// (O)^2
